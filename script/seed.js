10d97c7882db20ff69c35e6c82088301a7c4f981
celipas <celipas@gmail.com>
2019-01-30 15:08:07 -0600

    pulling to a new branch

--- script/seed.js
+++ script/seed.js
@@ -1,94 +1,94 @@
-// 'use strict'
+'use strict'
 
-// const db = require('../server/db')
-// const {User, Game} = require('../server/db/models')
+const db = require('../server/db')
+const {User, Game} = require('../server/db/models')
 
-// const gameData = [
-//   {
-//     modules: 5,
-//     modulesFailed: 2,
-//     startTime: 300,
-//     endTime: 120,
-//     strikesAllowed: 3,
-//     strikes: 2,
-//     status: 'passed'
-//   },
-//   {
-//     modules: 5,
-//     modulesFailed: 2,
-//     startTime: 300,
-//     endTime: 0,
-//     strikesAllowed: 3,
-//     strikes: 2,
-//     status: 'failed'
-//   },
-//   {
-//     modules: 5,
-//     modulesFailed: 3,
-//     startTime: 300,
-//     endTime: 57,
-//     strikesAllowed: 3,
-//     strikes: 2,
-//     status: 'failed'
-//   }
-// ]
+const gameData = [
+  {
+    modules: 5,
+    modulesFailed: 2,
+    startTime: 300,
+    endTime: 120,
+    strikesAllowed: 3,
+    strikes: 2,
+    status: 'passed'
+  },
+  {
+    modules: 5,
+    modulesFailed: 2,
+    startTime: 300,
+    endTime: 0,
+    strikesAllowed: 3,
+    strikes: 2,
+    status: 'failed'
+  },
+  {
+    modules: 5,
+    modulesFailed: 3,
+    startTime: 300,
+    endTime: 57,
+    strikesAllowed: 3,
+    strikes: 2,
+    status: 'failed'
+  }
+]
 
-// const userData = [
-//   {
-//     userName: 'dummyUser',
-//     email: 'dummyUser@dummyemail.com',
-//     password: 'dummypassword'
-//   },
-//   {
-//     userName: 'smartUser',
-//     email: 'smartUser@smartemail.com',
-//     password: 'smartpassword'
-//   }
-// ]
+const userData = [
+  {
+    userName: 'dummyUser',
+    email: 'dummyUser@dummyemail.com',
+    password: 'dummypassword'
+  },
+  {
+    userName: 'smartUser',
+    email: 'smartUser@smartemail.com',
+    password: 'smartpassword'
+  }
+]
 
-// async function seed() {
-//   await db.sync({force: true})
-//   console.log('db synced!')
+async function seed() {
+  await db.sync({force: true})
+  console.log('db synced!')
 
-//   const [game, user] = await Promise.all([
-//     Game.bulkCreate(gameData, {returning: true}),
-//     User.bulkCreate(userData, {individualHooks: true, returning: true})
-//   ])
+  const [game, user] = await Promise.all([
+    Game.bulkCreate(gameData, {returning: true}),
+    User.bulkCreate(userData, {individualHooks: true, returning: true})
+  ])
 
-//   const [game1, game2, game3] = game
-//   const [user1, user2] = user
+  const [game1, game2, game3] = game
+  const [user1, user2] = user
 
-//   await game1.setUser(user2)
-//   await game2.setUser(user1)
-//   await game3.setUser(user1)
+  await game1.setUser(user2)
+  await game2.setUser(user1)
+  await game3.setUser(user1)
 
-//   console.log(`seeded ${user.length} users`)
-//   console.log(`seeded successfully`)
-// }
+  console.log(`seeded ${user.length} users`)
+  console.log(`seeded successfully`)
+}
 
-// // We've separated the `seed` function from the `runSeed` function.
-// // This way we can isolate the error handling and exit trapping.
-// // The `seed` function is concerned only with modifying the database.
-// async function runSeed() {
-//   console.log('seeding...')
-//   try {
-//     await seed()
-//   } catch (err) {
-//     console.error(err)
-//     process.exitCode = 1
-//   } finally {
-//     console.log('closing db connection')
-//     await db.close()
-//     console.log('db connection closed')
-//   }
-// }
+// We've separated the `seed` function from the `runSeed` function.
+// This way we can isolate the error handling and exit trapping.
+// The `seed` function is concerned only with modifying the database.
+async function runSeed() {
+  console.log('seeding...')
+  try {
+    await seed()
+  } catch (err) {
+    console.error(err)
+    process.exitCode = 1
+  } finally {
+    console.log('closing db connection')
+    await db.close()
+    console.log('db connection closed')
+  }
+}
 
-// // Execute the `seed` function, IF we ran this module directly (`node seed`).
-// // `Async` functions always return a promise, so we can use `catch` to handle
-// // any errors that might occur inside of `seed`.
-// if (module === require.main) {
-//   runSeed()
-// }
+// Execute the `seed` function, IF we ran this module directly (`node seed`).
+// `Async` functions always return a promise, so we can use `catch` to handle
+// any errors that might occur inside of `seed`.
+if (module === require.main) {
+  runSeed()
+}
 
-// // we export the seed function for testing purposes (see `./seed.spec.js`)
-// module.exports = seed
+// we export the seed function for testing purposes (see `./seed.spec.js`)
+module.exports = seed
--- server/api/games.js
+++ server/api/games.js
@@ -1,15 +1,15 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const {Game} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await User.findAll({
+    const games = await Game.findAll({
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
     })
-    res.json(users)
+    res.json(games)
   } catch (err) {
     next(err)
   }
