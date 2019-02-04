df316bb2223a8f7ddd1910bd678929cdddc8cc72
June Idea <juneidea@yahoo.com>
2019-02-04 09:35:39 -0600

    one change:

--- client/components/Bomb/modules/LED.js
+++ client/components/Bomb/modules/LED.js
@@ -19,18 +19,3 @@ export const LEDcreate = (o, modules, name) => {
     shininess: 100
   })
 }
-
-export const CEDcreate = (o, modules, y) => {
-  let CED = new PointLight(0x006600, 5, 0.2, 2)
-  CED.name = o.name
-  CED.position.set(0.6, y, -0.7)
-  CED.visible = false
-  modules.add(CED)
-  o.material = new MeshPhongMaterial({
-    transparent: true,
-    opacity: 0.9,
-    emissive: new Color(0x000000),
-    color: new Color(0x006600),
-    shininess: 70
-  })
-}
