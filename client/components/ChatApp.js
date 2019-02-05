c405d8ad751c8ddd26e8937e35efc5c657bf8771
celipas <celipas@gmail.com>
2019-02-04 18:24:47 -0600

    semantic ui

--- client/components/ChatApp.js
+++ client/components/ChatApp.js
@@ -1,4 +1,13 @@
 import React, {Fragment} from 'react'
+import {
+  Grid,
+  Divider,
+  Image,
+  Segment,
+  Form,
+  Button,
+  Header
+} from 'semantic-ui-react'
 
 class ChatApp extends React.Component {
   constructor(props) {
@@ -13,12 +22,36 @@ class ChatApp extends React.Component {
     window.removeEventListener('load', this.handleLoad)
   }
 
+  comnponWillUpdate() {}
+
   handleLoad() {}
+
   render() {
+    const red = 'red'
     return (
       <Fragment>
-        <h1 className="ui header">Simple WebRTC Messenger</h1>
-        <hr />
+        <Header>Simple WebRTC Messenger</Header>
+        <Divider />
+        <Grid columns={10} relaxed="very" stackable>
+          <Grid.Column>
+            <Form>
+              <Form.Input
+                icon="user"
+                iconPosition="left"
+                label="Username"
+                placeholder="Username"
+              />
+              <Form.Input
+                icon="lock"
+                iconPosition="left"
+                label="Password"
+                type="password"
+              />
+
+              <Button content="Login" primary />
+            </Form>
+          </Grid.Column>
+        </Grid>
       </Fragment>
     )
   }
