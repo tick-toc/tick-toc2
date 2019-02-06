a87e9f84d110a476daa15a33ce063abff115f0bc
Evan Alto <evanalto@Evans-MacBook.local>
2019-02-06 13:10:57 -0600

    leaderboard and banner done

--- client/components/Leaderboard.js
+++ client/components/Leaderboard.js
@@ -31,36 +31,32 @@ class Leaderboard extends Component {
     return (
       <div className="leaderboard">
         <div className="leaders">
-          <div>
-            <div className="leaders--header">LEADERBOARD</div>
-            <table>
-              <tbody className="leaders--table">
-                <tr>
-                  <th>RANK</th>
-                  <th>PLAYER</th>
-                  <th>
-                    SOLVE<br /> TIME
-                  </th>
-                </tr>
-                <div className="leaders--main">
-                  {games.map((game, index) => {
-                    const {user: {userName}, finishTime, startTime} = game
-                    return (
-                      <tr
-                        className="leader-row"
-                        key={game.id}
-                        onClick={() => this.handleClick(index)}
-                      >
-                        <td>{index + 1}</td>
-                        <td>{userName}</td>
-                        <td>{calcSingleGameTime(startTime - finishTime)}</td>
-                      </tr>
-                    )
-                  })}
-                </div>
-              </tbody>
-            </table>
-          </div>
+          <div className="leaders--header">LEADERBOARD</div>
+          <table>
+            <tbody>
+              <tr className="leaders-table--headers">
+                <th>RANK</th>
+                <th>PLAYER</th>
+                <th>SOLVE TIME</th>
+              </tr>
+            </tbody>
+            <tbody className="leaders-table">
+              {games.map((game, index) => {
+                const {user: {userName}, finishTime, startTime} = game
+                return (
+                  <tr
+                    className="leader-row"
+                    key={game.id}
+                    onClick={() => this.handleClick(index)}
+                  >
+                    <td>{index + 1}</td>
+                    <td>{userName}</td>
+                    <td>{calcSingleGameTime(startTime - finishTime)}</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
           <button className="button" type="button">
             <Link to="/" className="return">
               BACK
