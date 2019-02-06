c8cde65724d3f01b44f9058d164a4cbf8c5ef4ce
Evan Alto <evanalto@Evans-MacBook.local>
2019-02-06 14:45:41 -0600

    new game menu

--- client/components/NewGame.js
+++ client/components/NewGame.js
@@ -46,51 +46,57 @@ class NewGame extends Component {
     const seconds = startTime % 60
     return (
       <div className="new-game-container">
-        <Link to="/" className="return">
-          Return to menu
-        </Link>
-        <div className="new-game-options">
-          <div>
-            <span>Time</span>
-            <span>
-              <FaArrowL
-                className="ng-action ng-icon"
-                onClick={() => this.handleTime('l')}
-              />
-              {minute}:{seconds === 0 ? '00' : seconds}
-              <FaArrowR
-                className="ng-action ng-icon"
-                onClick={() => this.handleTime('m')}
-              />
-            </span>
-          </div>
-          <div>
-            <span>Strikes</span>
+        <div>
+          <div className="new-game--config">
             <div>
-              <span
-                className={`${
-                  strikesAllowed ? 'strike ng-action' : 'ng-action'
-                }`}
-                onClick={() => this.handleStrikes('on')}
-              >
-                ON
-              </span>
-              <span
-                className={`${
-                  strikesAllowed ? 'ng-action' : 'strike ng-action'
-                }`}
-                onClick={() => this.handleStrikes('off')}
-              >
-                OFF
-              </span>
+              <div className="new-game--config-options">
+                <span>TIME</span>
+                <span>STRIKES</span>
+              </div>
+              <div className="new-game--config-selects">
+                <span>
+                  <FaArrowL
+                    className="ng-action ng-icon"
+                    onClick={() => this.handleTime('l')}
+                  />
+                  {minute}:{seconds === 0 ? '00' : seconds}
+                  <FaArrowR
+                    className="ng-action ng-icon"
+                    onClick={() => this.handleTime('m')}
+                  />
+                </span>
+                <div>
+                  <span
+                    className={`${
+                      strikesAllowed ? 'strikes--active' : 'strikes--inactive'
+                    }`}
+                    onClick={() => this.handleStrikes('on')}
+                  >
+                    ON
+                  </span>
+                  <span
+                    className={`${
+                      strikesAllowed ? 'strikes--inactive' : 'strikes--active'
+                    }`}
+                    onClick={() => this.handleStrikes('off')}
+                  >
+                    OFF
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="ng-action" onClick={this.handleStart}>
-            Start Game
+          <div className="new-game--select">
+            <Link to="/">
+              <button className="button" type="button">
+                BACK
+              </button>
+            </Link>
+            <div className="new-game--start" onClick={this.handleStart}>
+              START
+            </div>
           </div>
         </div>
-        {/* <h4>Let's Party</h4> */}
-        {/* <ChatApp /> */}
       </div>
     )
   }
@@ -110,3 +116,49 @@ const mapDispatch = dispatch => ({
 })
 
 export default connect(mapState, mapDispatch)(NewGame)
+
+//   < Link to = "/" className = "return" >
+//     Return to menu
+//         </Link >
+//   <div className="new-game-options">
+//     <div>
+//       <span>Time</span>
+//       <span>
+//         <FaArrowL
+//           className="ng-action ng-icon"
+//           onClick={() => this.handleTime('l')}
+//         />
+//         {minute}:{seconds === 0 ? '00' : seconds}
+//         <FaArrowR
+//           className="ng-action ng-icon"
+//           onClick={() => this.handleTime('m')}
+//         />
+//       </span>
+//     </div>
+//     <div>
+//       <span>Strikes</span>
+// <div>
+//   <span
+//     className={`${
+//       strikesAllowed ? 'strike ng-action' : 'ng-action'
+//       }`}
+//     onClick={() => this.handleStrikes('on')}
+//   >
+//     ON
+//         </span>
+//   <span
+//     className={`${
+//       strikesAllowed ? 'ng-action' : 'strike ng-action'
+//       }`}
+//     onClick={() => this.handleStrikes('off')}
+//   >
+//     OFF
+//         </span>
+// </div>
+//     </div>
+//     <div className="ng-action" onClick={this.handleStart}>
+//       Start Game
+//           </div>
+//   </div>
+// {/* <h4>Let's Party</h4> */ }
+// {/* <ChatApp /> */ }
