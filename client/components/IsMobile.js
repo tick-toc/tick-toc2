9c80f505344c15adb9079e9029f224a547b72116
Evan Alto <evanalto@Evans-MacBook.local>
2019-02-17 22:51:53 -0600

    isMobile ? not playable

---
+++ client/components/IsMobile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/IsMobile.css'
+
+const IsMobile = () => {
+  return (
+    <div className="is-mobile">
+      <span className="is-mobile--title">TICK-TOC</span>
+      <span>
+        Our apologies, this application is currently not playable on mobile or
+        tablet devices
+      </span>
+    </div>
+  )
+}
+
+export default IsMobile
