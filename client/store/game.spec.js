2dc0f25b7f55d964d8aecd419e6910f11d1cf727
Evan Alto <evanalto@Evans-MacBook.local>
2019-03-15 18:50:51 -0500

    added tests for action creators

--- client/store/game.spec.js
+++ client/store/game.spec.js
@@ -1,3 +1,104 @@
-import {startGame, saveGame} from './game'
+import Reducer, * as Game from './game'
+import {expect} from 'chai'
 
-describe('Game', () => {})
+describe('Game', () => {
+  describe('actions', () => {
+    describe('setStrike', () => {
+      it('should create an action to set a strike', () => {
+        expect(Game.setStrike()).to.deep.equal({type: Game.SET_STRIKE})
+      })
+    })
+
+    describe('startGame', () => {
+      it('should create an action to start a game', () => {
+        let settings = {
+          startTime: 300,
+          strikesAllowed: true,
+          strikeTotal: 3
+        }
+        expect(Game.startGame(settings)).to.deep.equal({
+          type: Game.START_GAME,
+          settings
+        })
+      })
+    })
+
+    describe('passModule', () => {
+      it('should create an action to pass a module', () => {
+        let moduleName = 'Wires'
+        expect(Game.passModule(moduleName)).to.deep.equal({
+          type: Game.PASS_MODULE,
+          moduleName
+        })
+      })
+    })
+
+    describe('diffused', () => {
+      it('should create an action to mark diffused', () => {
+        expect(Game.diffused()).to.deep.equal({type: Game.DIFFUSED})
+      })
+    })
+
+    describe('endGame', () => {
+      it('should create an action to end a game', () => {
+        let status = 'diffused'
+        let finishTime = 24
+        expect(Game.endGame(status, finishTime)).to.deep.equal({
+          type: Game.END_GAME,
+          status,
+          finishTime
+        })
+      })
+    })
+
+    describe('resetGame', () => {
+      it('should create an action to reset a game', () => {
+        expect(Game.resetGame()).to.deep.equal({type: Game.RESET_GAME})
+      })
+    })
+
+    describe('replayGame', () => {
+      it('should create an action to replay a game', () => {
+        expect(Game.replayGame()).to.deep.equal({type: Game.REPLAY_GAME})
+      })
+    })
+
+    describe('getUserGames', () => {
+      it('should create an action to get a users games', () => {
+        let data = {}
+        expect(Game.getUserGames(data)).to.deep.equal({
+          type: Game.GET_USER_GAMES,
+          data
+        })
+      })
+    })
+
+    describe('getLeaders', () => {
+      it('should create an action to get leaders', () => {
+        let data = {}
+        expect(Game.getLeaders(data)).to.deep.equal({
+          type: Game.GET_LEADERS,
+          data
+        })
+      })
+    })
+
+    describe('isDoneFetching', () => {
+      it('should create an action to indicate fetching is done', () => {
+        let view = 'leaders'
+        expect(Game.isDoneFetching(view)).to.deep.equal({
+          type: Game.IS_DONE_FETCHING,
+          view
+        })
+      })
+    })
+  })
+
+  describe('thunks', () => {
+    describe('saveGame', () => {})
+
+    describe('fetchLeaders', () => {})
+
+    describe('fetchUserGames', () => {})
+  })
+})
--- client/store/user.spec.js
+++ client/store/user.spec.js
@@ -1,7 +1,7 @@
 /* global describe beforeEach afterEach it */
 
 import {expect} from 'chai'
-import {me, logout} from './user'
+import Reducer, * as User from './user'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import configureMockStore from 'redux-mock-store'
@@ -11,42 +11,66 @@ import history from '../history'
 const middlewares = [thunkMiddleware]
 const mockStore = configureMockStore(middlewares)
 
-describe('thunk creators', () => {
-  let store
-  let mockAxios
-
-  const initialState = {user: {}}
-
-  beforeEach(() => {
-    mockAxios = new MockAdapter(axios)
-    store = mockStore(initialState)
-  })
-
-  afterEach(() => {
-    mockAxios.restore()
-    store.clearActions()
-  })
-
-  describe('me', () => {
-    it('eventually dispatches the GET USER action', async () => {
-      const fakeUser = {email: 'Cody'}
-      mockAxios.onGet('/auth/me').replyOnce(200, fakeUser)
-      await store.dispatch(me())
-      const actions = store.getActions()
-      expect(actions[0].type).to.be.equal('IS_FETCHING')
-      expect(actions[1].type).to.be.deep.equal('GET_USER')
-      expect(actions[1].user).to.be.deep.equal(fakeUser)
-      expect(actions[2].type).to.be.equal('IS_FETCHING')
+describe('User', () => {
+  describe('actions', () => {
+    describe('getUser', () => {
+      it('should create an action to get a user', () => {
+        let user = {
+          username: 'test',
+          password: 'test'
+        }
+        expect(User.getUser(user)).to.deep.equal({type: User.GET_USER, user})
+      })
+    })
+    describe('removeUser', () => {
+      it('should create an action to remove a user', () => {
+        expect(User.removeUser()).to.deep.equal({type: User.REMOVE_USER})
+      })
+    })
+    describe('isFetching', () => {
+      it('should create an action for fetching', () => {
+        expect(User.isFetching()).to.deep.equal({type: User.IS_FETCHING})
+      })
     })
   })
 
-  describe('logout', () => {
-    it('logout: eventually dispatches the REMOVE_USER action', async () => {
-      mockAxios.onPost('/auth/logout').replyOnce(204)
-      await store.dispatch(logout())
-      const actions = store.getActions()
-      expect(actions[0].type).to.be.equal('REMOVE_USER')
-      expect(history.location.pathname).to.be.equal('/login')
+  describe('thunks', () => {
+    let store
+    let mockAxios
+
+    const initialState = {user: {}}
+
+    beforeEach(() => {
+      mockAxios = new MockAdapter(axios)
+      store = mockStore(initialState)
+    })
+
+    afterEach(() => {
+      mockAxios.restore()
+      store.clearActions()
+    })
+
+    describe('me', () => {
+      it('eventually dispatches the GET USER action', async () => {
+        const fakeUser = {email: 'Cody'}
+        mockAxios.onGet('/auth/me').replyOnce(200, fakeUser)
+        await store.dispatch(User.me())
+        const actions = store.getActions()
+        expect(actions[0].type).to.be.equal('IS_FETCHING')
+        expect(actions[1].type).to.be.deep.equal('GET_USER')
+        expect(actions[1].user).to.be.deep.equal(fakeUser)
+        expect(actions[2].type).to.be.equal('IS_FETCHING')
+      })
+    })
+
+    describe('logout', () => {
+      it('logout: eventually dispatches the REMOVE_USER action', async () => {
+        mockAxios.onPost('/auth/logout').replyOnce(204)
+        await store.dispatch(User.logout())
+        const actions = store.getActions()
+        expect(actions[0].type).to.be.equal('REMOVE_USER')
+        expect(history.location.pathname).to.be.equal('/login')
+      })
     })
   })
 })
